7a6c73c714e7e115d99807de2fca23ac8880d80d
Michał Śledź <michalsledz34@gmail.com>
2022-11-18 14:54:41 +0100

    Bump version to 0.6.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.Format.MixProject do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_format"
 
   def project do
