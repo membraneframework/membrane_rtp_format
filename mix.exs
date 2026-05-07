9ae33fc5f58f91d1d69337f5e2b88b1165f746dc
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:06 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -45,16 +45,15 @@ defmodule Membrane.RTP.Format.MixProject do
   defp deps do
     [
       {:membrane_core, "~> 1.0"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
